29a477f99155cbe75ac0f8a946f369f0e4d8e1b3
login <w0nderfly@live.com>
2022-11-14 12:58:45 +0500

    first commit

---
+++ src/main/java/com/example/course3/employee/EmployeeBookSpring2Application.java
@@ -0,0 +1,13 @@
+package com.example.course3.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course3/employee/EmployeeBookSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course3.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
